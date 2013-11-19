bc05664ed4bed0fb492bc0b00b666ea44251d200
Jacques Atlas <apps@moofia.net>
2013-11-19 08:56:36 +0200

    fixed debugging

--- lib/base.rb
+++ lib/base.rb
@@ -93,8 +93,8 @@ end
 def processSummaries
   # process all the summaries
   @Device.interfaces.each do |interface|
-    #interface.connected_routes.to_routes_summary if $opt["mode"] == "routes"
-    puts "#{interface.name} --> #{interface.description}"
+    interface.connected_routes.to_routes_summary if $opt["mode"] == "routes"
+    #puts "#{interface.name} --> #{interface.description}"
   end
   @Device.summarizeStaticRoutes if $opt["mode"] == "routes"
   
